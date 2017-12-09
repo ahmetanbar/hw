f27e5d3228b11520befb78c4a9596a79ac8903b0
Ahmet <ahmetanbar@hacettepe.edu.tr>
2017-12-10 00:49:45 +0200

    library eklendi

--- turkcekarakter.py
+++ turkcekarakter.py
@@ -1,17 +1,10 @@
+#--coding:cp1254
 
-# -*- coding: utf-8 -*-
-
-from reportlab.pdfgen import canvas 
-from reportlab.pdfbase import pdfmetrics 
-from reportlab.pdfbase.ttfonts import TTFont 
-
-pdfmetrics.registerFont(TTFont('Hebrew', 'Arial.ttf'))
-
-c = canvas.Canvas("hello.pdf") 
-c.setFont("Hebrew", 14)
-c.drawString(10,10, u"þelale".encode('utf-8'))
-c.save()
-
+#kutuphaneler
+from Tkinter import *
+import tkMessageBox
+import random
+import tkFont
 import requests
 
 url="https://poloniex.com/public?command=returnTicker"
