45ae51a4bb135bc6532ca0c8c2677f0708119f56
Kaan Arı <kaanari@hacettepe.edu.tr>
2017-12-12 00:06:39 +0300

    Finish
    -e number in floats is fixed.
    -unnecessary delay and while is deleted.

--- hw1/currency-hw1-kaan.py
+++ hw1/currency-hw1-kaan.py
@@ -1,15 +1,8 @@
 import requests
-import time
 api1="https://bittrex.com/api/v1.1/public/getmarketsummaries"
 type_coin=input("Oranlarını Görmek İstediğiniz Coin Türünü Giriniz:")
 response=requests.get(api1)
 json_verisi=response.json()
-while(1):
-    for i in range(0,250):
-        if type_coin==json_verisi["result"][i]["MarketName"][:3]:
-                print("1",json_verisi["result"][i]["MarketName"][4:]," == ",json_verisi["result"][i]["Last"]," BTC")
-                time.sleep(1/2)
-print("""
-=====================DEĞERLER GÜNCELLENİYOR========================
-""")
-time.sleep(5)
+for i in range(0,250):
+    if type_coin==json_verisi["result"][i]["MarketName"][:3]:
+            print("1",json_verisi["result"][i]["MarketName"][4:]," == ",format(json_verisi["result"][i]["Last"], '.8f'),type_coin)
