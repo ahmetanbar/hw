2f3c28e581e81d4b2bbcb70b6c17b95559a4c188
Kaan Arı <kaanari@hacettepe.edu.tr>
2017-12-12 17:11:33 +0300

    Bug Fixed

--- hw2/currency-hw1-kaan.py
+++ hw2/currency-hw1-kaan.py
@@ -5,8 +5,12 @@ api="https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-ETH"
 response=requests.get(api)
 json=response.json()
 a=0
-times=[0 for i in range(100)]
-values=[0 for i in range(100)]
+for i in range(0,100):
+    if "BUY" == str(json["result"][i]["OrderType"]):
+        a=a+1
+times=[0 for i in range(a)]
+values=[0 for i in range(a)]
+a=0
 for i in range(0,100):
     if "BUY"==str(json["result"][i]["OrderType"]):
         times[a]=str(json["result"][i]["TimeStamp"])
