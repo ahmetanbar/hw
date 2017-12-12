816d8baa7496d57fb7c00cb3d45d3c68a26393cf
Kaan Arı <kaanari@hacettepe.edu.tr>
2017-12-12 17:03:27 +0300

    First Step
    All Times and Prices is written in Array for matplotlib plt.plot command.

--- hw2/currency-hw1-kaan.py
+++ hw2/currency-hw1-kaan.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 import requests
 import numpy as np
-api="https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-DOGE"
+api="https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-ETH"
 response=requests.get(api)
 json=response.json()
 a=0
 times=[0 for i in range(100)]
+values=[0 for i in range(100)]
 for i in range(0,100):
     if "BUY"==str(json["result"][i]["OrderType"]):
         times[a]=str(json["result"][i]["TimeStamp"])
+        values[a]=format(json["result"][i]["Price"],'.8f')
         a=a+1
 print(times[:])
+print(values[:])
