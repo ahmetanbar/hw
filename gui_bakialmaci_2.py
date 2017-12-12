b850a933c5570f417378a3a39d9b86c97e12ac39
bakialmaci <bakialmaci@gmail.com>
2017-12-12 17:19:05 +0300

    Beta.2 Arayüz geliştirmeleri öğreniliyor.

--- gui_bakialmaci_2.py
+++ gui_bakialmaci_2.py
@@ -21,7 +21,6 @@ pswd = Label(window,text = "Password: ")
 button = Button(window,text = "Login",command = login_verify)
 #button.bind("<Button-1>",push)
 
-
 user.grid(row = 0,column = 0,sticky = E) #row = satır ---- W - E - N - S.
 pswd.grid(row = 1,column = 0,sticky = E) #column = sütun  ----- sticky yapıstırır.
 button.grid(row = 2,column = 1)
@@ -35,7 +34,5 @@ entry_2.grid(row = 1,column = 1)
 
 remember = Checkbutton(window,text = "Don`t forget me ")
 remember.grid(columnspan = 2) #columnspan = kaç sütunun altına koyulacağını ayarlar.(ortaya yapar)
-
-login_verify()
 
 window.mainloop() #window açık.
