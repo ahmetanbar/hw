cde740a58a86f3e8db990d1a6db33aa604d2e043
Kaan Arı <33496550+kaanaritr@users.noreply.github.com>
2017-12-13 19:54:10 +0300

    Data Library Finished
    Data Library Finished.
    -Commits added.

--- hw2/graph.py
+++ hw2/graph.py
@@ -1,28 +1,27 @@
-from data import *
+from data import * # yazdığımız data kütüphanesindeki tüm fonksiyonları çektik
 import matplotlib.pyplot as plt
-from matplotlib import style,pyplot
-import datetime
-import matplotlib.dates as mdates
+from matplotlib import style # matplotlib in style fonksiyonu çekildi
+import datetime # datetime kütüphanesi eklendi
+import matplotlib.dates as mdates #matplotlib e dateler yazdırmak için kütüphane eklendi
 def graph_current(type1,type2): # Yakın Zaman Grafiğini Veriyor.
-    style.use('ggplot')
-    plt.figure(figsize=(15, 10), dpi=50,num=(20))
-    plt.title(type1+" - "+type2+" Latest History Chart")
-    plt.xticks(rotation=90)
-    plt.plot(times_history(type1,type2),values_history(type1,type2))
-    plt.show()
+    style.use('ggplot') #ggplot tasarımındaki tablomuzun stylenı belirledik.
+    plt.figure(figsize=(15, 10), dpi=50,num=(20))#Tablodaki verilerin font ayarlarını yaptık.
+    plt.title(type1+" - "+type2+" Latest History Chart")#Tablo Başlığını Kullanıcıan Aldığımız Değerlere göre yazdırdık.
+    plt.xticks(rotation=90)#x eksenindeki gelen verileri daha rahat görünüp üst üste binmemesi için rotate ettik.
+    plt.plot(times_history(type1,type2),values_history(type1,type2))#(x,y) formatında değişkenlerimizi tabloya gönderdik.
+    plt.show()#tabloyu gösterdik.
+    plt.close()
 def graph_history(): # BTC-USD Geçmiş Zaman Grafiğini Veriyor
     style.use('ggplot')
     plt.figure(figsize=(15, 10), dpi=50, num=(20))
-    title1="BTC - USD History Chart\nCurrent Buy Price ="+str(btc_usd_current())
+    title1="BTC - USD History Chart\nCurrent Buy Price ="+str(btc_usd_current())#Üsttekine ek Current Valuelarda Başlığa Eklendi.
     plt.title(title1)
     plt.xticks(rotation=45)
-    x = [datetime.datetime.now() - datetime.timedelta(days=i) for i in range(2699)]
-    y = values_btc_usd_alltime()
+    x = [datetime.datetime.now() - datetime.timedelta(days=i) for i in range(2699)]#datetime kütüphanesi kullanılarak şuanki zamanda günlük olarak geriye doğru elimizdeki değer kadar değeri array e yazdırdık.
+    y = values_btc_usd_alltime() # data kütüphanemizden çektiğimiz değerleri y eksenine bastırdık.
     plt.plot(x, y)
-    if times_btc_usd_alltime()==str(x):
-        plt.scatter(x, y)
     plt.gcf().autofmt_xdate()
-    myFmt = mdates.DateFormatter('%d:%m:%Y')
+    myFmt = mdates.DateFormatter('%d:%m:%Y') # Datatime daki değerlerin hangi formatta yazdırılacağını seçtik.
     plt.gca().xaxis.set_major_formatter(myFmt)
     plt.show()
     plt.close()
