103d28327b3c2d4350b0ecb5713b327d6ff41b01
bakialmaci <bakialmaci@gmail.com>
2017-12-13 14:38:36 +0300

    Parsing Time Json Datas _ BETA

--- gui_bakialmaci_2.py
+++ gui_bakialmaci_2.py
@@ -1,25 +1,34 @@
 from tkinter import *
+from tkinter import Tk
+from graph import *
 
 window = Tk()
 window.title("GUI Interface BETA")
-window.geometry("250x100")
-#window.configure(background = "red")
+
+window.withdraw()
+window.update_idletasks()  # Update "requested size" from geometry manager
+
+x = (window.winfo_screenwidth() - window.winfo_reqwidth()) / 2
+y = (window.winfo_screenheight() - window.winfo_reqheight()) / 2
+window.geometry("+%d+%d" % (x, y))
+
 def login():
-    print("Not yet :)")
+    print("Not Found !")
 
 def login_verify():
     user = entry_1.get()
     pswd = entry_2.get()
-    if user == "baki" and pswd == "123":
-        print("As soon as")
-        window.configure(background="green")
-    else:
-        login()
+    graph(user,pswd)
 
-user = Label(window,text = "Username: ")
-pswd = Label(window,text = "Password: ")
-button = Button(window,text = "Login",command = login_verify)
-#button.bind("<Button-1>",push)
+user = Label(window,text = "1.Currency Name: ")
+pswd = Label(window,text = "2.Currency Name: ")
+button = Button(window,text = "Show Graph",command = login_verify)
+
+Lb = Listbox(window,selectmode=EXTENDED)
+Lb.insert(1,"BTC")
+Lb.insert(2,"ETH")
+Lb.insert(3,"LTC")
+Lb.pack()
 
 user.grid(row = 0,column = 0,sticky = E) #row = satır ---- W - E - N - S.
 pswd.grid(row = 1,column = 0,sticky = E) #column = sütun  ----- sticky yapıstırır.
@@ -31,8 +40,5 @@ entry_2 = Entry(window)
 entry_1.grid(row = 0,column = 1)
 entry_2.grid(row = 1,column = 1)
 
-
-remember = Checkbutton(window,text = "Don`t forget me ")
-remember.grid(columnspan = 2) #columnspan = kaç sütunun altına koyulacağını ayarlar.(ortaya yapar)
-
+window.deiconify()
 window.mainloop() #window açık.
