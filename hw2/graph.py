860f1fdd2bec6e1d61e64a2f5f207fea5a09efaa
Kaan Arı <33496550+kaanaritr@users.noreply.github.com>
2017-12-17 22:32:34 +0300

    Update graph.py

--- hw2/graph.py
+++ hw2/graph.py
@@ -1,29 +1,39 @@
-from data import * # yazdığımız data kütüphanesindeki tüm fonksiyonları çektik asd
-import matplotlib
-matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-from matplotlib import style # matplotlib in style fonksiyonu çekildi
-import datetime # datetime kütüphanesi eklendi
+from data import *
+import matplotlib.animation as animation
 import matplotlib.dates as mdates #matplotlib e dateler yazdırmak için kütüphane eklendi
-def graph_current(type1,type2): # Yakın Zaman Grafiğini Veriyor.
+from matplotlib import style
+def graph_latest(type1,type2):
+    fig=plt.figure(figsize=(15, 10), dpi=50,num=20)
     style.use('ggplot') #ggplot tasarımındaki tablomuzun stylenı belirledik.
-    plt.figure(figsize=(15, 10), dpi=50,num=(20))#Tablodaki verilerin font ayarlarını yaptık.
-    plt.title(type1+" - "+type2+" Latest History Chart")#Tablo Başlığını Kullanıcıan Aldığımız Değerlere göre yazdırdık.
-    plt.xticks(rotation=90)#x eksenindeki gelen verileri daha rahat görünüp üst üste binmemesi için rotate ettik.
-    plt.plot(times_history(type1,type2),values_history(type1,type2))#(x,y) formatında değişkenlerimizi tabloya gönderdik.
-    plt.show()#tabloyu gösterdik.
-    plt.close()
-def graph_history(): # BTC-USD Geçmiş Zaman Grafiğini Veriyor
-    style.use('ggplot')
-    plt.figure(figsize=(15, 10), dpi=50, num=(20))
-    title1="BTC - USD History Chart\nCurrent Buy Price ="+str(btc_usd_current())#Üsttekine ek Current Valuelarda Başlığa Eklendi.
-    plt.title(title1)
-    plt.xticks(rotation=45)
-    x = [datetime.datetime.now() - datetime.timedelta(days=i) for i in range(2699)]#datetime kütüphanesi kullanılarak şuanki zamanda günlük olarak geriye doğru elimizdeki değer kadar değeri array e yazdırdık.
-    y = values_btc_usd_alltime() # data kütüphanemizden çektiğimiz değerleri y eksenine bastırdık.
-    plt.plot(x, y)
-    plt.gcf().autofmt_xdate()
-    myFmt = mdates.DateFormatter('%d:%m:%Y') # Datatime daki değerlerin hangi formatta yazdırılacağını seçtik.
-    plt.gca().xaxis.set_major_formatter(myFmt)
+    ax1=fig.add_subplot(1,1,1)
+    def animate(i):
+        time, value = current(type1, type2)
+        title = type1 + " - " + type2 + " Latest History Chart\nCurrent Price = " +str(value)
+        ax1.set_title(title)
+        plt.xticks(rotation=90,size=15)  # x eksenindeki gelen verileri daha rahat görünüp üst üste binmemesi için rotate ettik.
+        plt.gcf().autofmt_xdate()
+        myFmt = mdates.DateFormatter('%H:%M:%S')  # Datatime daki değerlerin hangi formatta yazdırılacağını seçtik.
+        plt.gca().xaxis.set_major_formatter(myFmt)
+        x, y=history(type1,type2)
+        #ax1.clear() Eğer değerler olan grafiğe eklenirse bu eklenmeyecek.
+        ax1.plot(x,y,color='red')
+    ani=animation.FuncAnimation(fig, animate,interval=1000)
     plt.show()
-    plt.close()
+#graph_current("BTC","LTC")
+def graph_usd_x_alltime(type1):
+    fig=plt.figure(figsize=(15,10), dpi=50, num=20)
+    style.use('ggplot')
+    ax1=fig.add_subplot(1,1,1)
+    def animate(i):
+        time, value = current("USDT",type1)
+        title ="USD - " + type1 + " All Time History Chart\nCurrent Price = " + str(value)
+        ax1.set_title(title)
+        plt.xticks(rotation=90,size=15)
+        plt.gcf().autofmt_xdate()
+        myFmt=mdates.DateFormatter('%H:%M:%S')
+        plt.gca().xaxis.set_major_formatter(myFmt)
+        x, y =usd_x_alltime(type1)
+        ax1.plot(x,y,color='red')
+    ani=animation.FuncAnimation(fig,animate,interval=1000)
+    plt.show()
