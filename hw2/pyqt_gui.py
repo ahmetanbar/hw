4c399b4560b41200dc17117d2617c031ea992d06
bakialmaci <bakialmaci@gmail.com>
2017-12-22 01:40:49 +0300

    bazı hatalar ve eklenecek özellikler var

--- hw2/pyqt_gui.py
+++ hw2/pyqt_gui.py
@@ -1,8 +1,7 @@
 import sys
 from graph import *
 from data import *
-from PyQt5.QtWidgets import QDialog, QApplication,QMessageBox, QPushButton,QHBoxLayout, QListWidget, QLineEdit,QProgressBar
-from PyQt5.QtCore import QBasicTimer
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton,QHBoxLayout,QListWidget,QMessageBox, QLineEdit
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 #############################################################################################################################################
 
@@ -20,21 +19,14 @@ class Window(QDialog):
         self.setWindowTitle('GUI 2018')
         self.listWidget = QListWidget(self)
 
-        self.pbar = QProgressBar(self)
-        self.pbar.setGeometry(4,10,185,25)
-        self.timer = QBasicTimer()
-
         self.button = QPushButton('Search and Plot',self)
-        self.button.clicked.connect(self.graph_latest)
+        self.button.clicked.connect(self.search)
         self.button2 = QPushButton('BTC',self)
         self.button2.clicked.connect(self.addlistbtc)
         self.button3 = QPushButton('ETH',self)
         self.button3.clicked.connect(self.addlisteth)
-        self.button4 = QPushButton('USD',self)
+        self.button4 = QPushButton('USDT',self)
         self.button4.clicked.connect(self.addlistusdt)
-        self.textbox = QLineEdit(self)
-        self.textbox.move(4,405)
-        self.textbox.resize(150,30)
         self.button.move(4,435)
         self.button.resize(150,20)
         self.button2.move(4,455)
@@ -43,10 +35,15 @@ class Window(QDialog):
         self.button3.resize(50,38)
         self.button4.move(104,455)
         self.button4.resize(50,38)
-        self.listWidget.move(4,50)
-        self.listWidget.resize(150,350)
+        self.listWidget.move(4,10)
+        self.listWidget.resize(150,390)
         self.listWidget.itemClicked.connect(self.Clicked)
-        self.step = 0
+        self.textbox = QLineEdit(self)
+        self.textbox.move(4,405)
+        self.textbox.resize(150,30)
+
+
+
 
         self.figure = plt.figure(figsize=(15, 10), dpi=50, num=20)
         self.canvas = FigureCanvas(self.figure)
@@ -56,6 +53,44 @@ class Window(QDialog):
         graphbox.addWidget(self.canvas)
         self.setLayout(graphbox)
 
+    def search(self):
+        global a
+        global items
+        global type1
+        global select1
+        a = str(self.textbox.text())
+        print(a.upper())
+
+        items=[]
+        s = 0
+        for i in range(self.listWidget.count()):
+            s+=1
+
+        if s == 0:
+            buttonReply = QMessageBox.question(self, 'Beklenen HATA !!!', "Bir dahaki sefere Listeyi doldurmadan basmayacağıma söz veriyorum.",
+                                    QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            if buttonReply == QMessageBox.Yes:
+                self.close()
+            else:
+                print(" Designed by bakialmaci ")
+
+
+
+        for i in range(self.listWidget.count()):
+            if a.upper() == self.listWidget.item(i).text() and type1 == "BTC":
+                select1 = a.upper()
+                self.graph_latest2()
+            elif a.upper() == self.listWidget.item(i).text() and type1 == "ETH":
+                select1 = a.upper()
+                self.graph_latest2()
+            elif a.upper() == self.listWidget.item(i).text() and type1 == "USDT":
+                select1 = a.upper()
+                self.graph_usd_x_alltime2()
+
+            print(self.listWidget.item(i).text())
+
+
+
     def addlistbtc(self):
         global type1
         eth,btc,usdt= names_eth_btc_usdt()
@@ -64,10 +99,7 @@ class Window(QDialog):
         type1 = "BTC"
         print(type1)
         self.canvas.close_event()
-        self.pbar.setValue(0)
-        for i in range(100):
-            self.pbar.setValue(self.step)
-            self.step= i+2
+        self.figure.clear()
 
 
     def addlisteth(self):
@@ -78,34 +110,35 @@ class Window(QDialog):
         type1 = "ETH"
         print(type1)
         self.canvas.close_event()
-        self.pbar.setValue(0)
-        for i in range(100):
-            self.pbar.setValue(self.step)
-            self.step= i+2
+        self.figure.clear()
 
     def addlistusdt(self):
-
         global type1
         eth,btc,usdt= names_eth_btc_usdt()
         self.listWidget.clear()
         self.listWidget.addItems(usdt)
         type1 = "USDT"
-        self.canvas.close_event()
         print(type1)
-        self.pbar.setValue(0)
-        for i in range(100):
-            self.pbar.setValue(self.step)
-            self.step= i+2
-
+        self.canvas.close_event()
+        self.figure.clear()
 
     def Clicked(self, item):
-        # QMessageBox.information(self, "ListWidget", "You clicked: " + item.text())
-        print(item.text())
-
+        global type1
+        if type1 == "BTC":
+            # print(item.text())
+            self.figure.clear()
+            self.graph_latest()
+        elif type1 == "ETH":
+            # print(item.text())
+            self.figure.clear()
+            self.graph_latest()
+        elif type1== "USDT":
+            # print(item.text())
+            self.figure.clear()
+            self.graph_usd_x_alltime()
 
 
     def graph_latest(self):
-        self.canvas.close_event()
         global select1
         select1=self.listWidget.currentItem().text()
         print(select1)
@@ -124,12 +157,50 @@ class Window(QDialog):
             x, y = history(type1, select1)
             ax1.plot(x, y, color='blue')
 
-        ani = animation.FuncAnimation(self.figure, animate, interval=3000)
-        self.pbar.setValue(0)
-        for i in range(100):
-            self.pbar.setValue(self.step)
-            self.step= i+2
+        ani = animation.FuncAnimation(self.figure, animate, interval=1000)
+        self.canvas.draw()
 
+
+    def graph_latest2(self):
+        global select1
+        print(select1)
+
+        style.use('ggplot')  # ggplot tasarımındaki tablomuzun stylenı belirledik.
+        ax1 = self.figure.add_subplot(1, 1, 1)
+
+        def animate(i):
+            time, value = current(type1,select1)
+            title = type1 + " - " + select1 + " Latest History Chart\nCurrent Price = " + str(value)
+            ax1.set_title(title)
+            plt.xticks(rotation=90,size=15)  # x eksenindeki gelen verileri daha rahat görünüp üst üste binmemesi için rotate ettik.
+            plt.gcf().autofmt_xdate()
+            myFmt = mdates.DateFormatter('%H:%M:%S')  # Datatime daki değerlerin hangi formatta yazdırılacağını seçtik.
+            plt.gca().xaxis.set_major_formatter(myFmt)
+            x, y = history(type1, select1)
+            ax1.plot(x, y, color='blue')
+
+        ani = animation.FuncAnimation(self.figure, animate, interval=1000)
+        self.canvas.draw()
+
+    def graph_usd_x_alltime2(self):
+
+        global select1
+        print(select1)
+        style.use('ggplot')
+        ax2 = self.figure.add_subplot(1, 1, 1)
+
+        def animate(i):
+            time, value = current("USDT", select1)
+            title = "USD - " + select1 + " All Time History Chart\nCurrent Price = " + str(value)
+            ax2.set_title(title)
+            plt.xticks(rotation=90, size=15)
+            plt.gcf().autofmt_xdate()
+            myFmt = mdates.DateFormatter('%H:%M:%S')
+            plt.gca().xaxis.set_major_formatter(myFmt)
+            x, y = usd_x_alltime(select1)
+            ax2.plot(x, y, color='green')
+
+        ani = animation.FuncAnimation(self.figure, animate, interval=3000)
         self.canvas.draw()
 
     def graph_usd_x_alltime(self):
@@ -137,20 +208,19 @@ class Window(QDialog):
         global select1
         select1=self.listWidget.currentItem().text()
         print(select1)
-
         style.use('ggplot')
-        ax1 = self.figure.add_subplot(1, 1, 1)
+        ax2 = self.figure.add_subplot(1, 1, 1)
 
-        def animate():
-            time, value = current("USDT", "ETH")
-            title = "USD - " + "ETH" + " All Time History Chart\nCurrent Price = " + str(value)
-            ax1.set_title(title)
+        def animate(i):
+            time, value = current("USDT", select1)
+            title = "USD - " + select1 + " All Time History Chart\nCurrent Price = " + str(value)
+            ax2.set_title(title)
             plt.xticks(rotation=90, size=15)
             plt.gcf().autofmt_xdate()
             myFmt = mdates.DateFormatter('%H:%M:%S')
             plt.gca().xaxis.set_major_formatter(myFmt)
-            x, y = usd_x_alltime("ETH")
-            ax1.plot(x, y, color='blue')
+            x, y = usd_x_alltime(select1)
+            ax2.plot(x, y, color='green')
 
         ani = animation.FuncAnimation(self.figure, animate, interval=3000)
         self.canvas.draw()
