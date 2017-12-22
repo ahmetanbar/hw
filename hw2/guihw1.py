926a6ac4d5550f07ecf0fc647db8b21bcedb58f5
Ahmet <ahmetanbar@hacettepe.edu.tr>
2017-12-22 14:26:17 +0200

    arama eklendi.arama kısmı hataları ayarlandı.

--- hw2/guihw1.py
+++ hw2/guihw1.py
@@ -1,6 +1,6 @@
 import sys
 import requests
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout,QHBoxLayout, QListView, QListWidget, QLineEdit, QLabel
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout,QHBoxLayout, QListView, QListWidget, QLineEdit, QLabel,QMessageBox
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
@@ -72,14 +72,10 @@ class Window(QDialog):
         layout.addWidget(self.canvas)
         self.setLayout(layout)
 
-
-
     def touchme(self):
         global ss
-        global benim
         ss= self.listWidget.currentItem().text()
         print(ss)
-
 
     def buttonsee(self,button_id):
         global choose
@@ -94,25 +90,51 @@ class Window(QDialog):
             self.nameUSD()
 
     def plot(self):
+        global ss
         xList= []
         yList= []
 
         summaryurl = "https://bittrex.com/api/v1.1/public/getmarkethistory?market="
-        global ss
+        # if not :
+        #     print("ss girilmedi")
+        print("xx")
         if ("BTC"==choose):
-            if (self.textbox.text()!=""):
+            if (self.textbox.text() in btcList):
                 ss=self.textbox.text()
                 self.textbox.clear()
+                print("icerdeyim")
+            elif (self.textbox.text()!="") :
+                textboxValue = self.textbox.text()
+                self.textbox.clear()
+                QMessageBox.question(self, 'what the!!!', "Onca seçenek varken neden \" " + textboxValue + "\" anlatir misin? \n      Belki ETH'ye bakmak istersin.", QMessageBox.Ok,
+                                     QMessageBox.Ok)
+
+                ss = "ETH"
             summaryurl=summaryurl+"BTC-"+ ss
         elif ("ETH"==choose):
-            if (self.textbox.text()!=""):
+            if (self.textbox.text() in ethList):
                 ss=self.textbox.text()
                 self.textbox.clear()
+                print("icerdeyim")
+            elif (self.textbox.text()!="") :
+                textboxValue = self.textbox.text()
+                self.textbox.clear()
+                QMessageBox.question(self, 'what the!!!', "Onca seçenek varken neden \" " + textboxValue + "\" anlatir misin? \n      Belki ETC'ye bakmak istersin.", QMessageBox.Ok,
+                                     QMessageBox.Ok)
+
+                ss = "ETC"
             summaryurl=summaryurl+"ETH-"+ ss
         elif ("USDT"==choose):
-            if (self.textbox.text()!=""):
+            if (self.textbox.text() in usdList):
                 ss=self.textbox.text()
                 self.textbox.clear()
+                print("icerdeyim")
+            elif (self.textbox.text()!="") :
+                textboxValue = self.textbox.text()
+                self.textbox.clear()
+                QMessageBox.question(self, 'what the!!!', "Onca seçenek varken neden \" " + textboxValue + "\" anlatir misin? \n      Belki BTC'ye bakmak istersin.", QMessageBox.Ok,
+                                     QMessageBox.Ok)
+                ss = "BTC"
             summaryurl=summaryurl+"USDT-"+ ss
         summary = requests.get(summaryurl)
         json_summary = summary.json()
@@ -123,18 +145,20 @@ class Window(QDialog):
             if k<20:
                 m = json_summary["result"][k]["Price"]
                 n = json_summary["result"][k]["TimeStamp"][14:19]
-                xList.append(float(m))
+                xList.append(str(m))
                 yList.append(str(n))
-
         self.figure.clear()
         ax = self.figure.add_subplot(111)
         ax.plot(yList,xList, 'o-')
-        ax.set_title("Last Price= {:f}".format(json_summary["result"][0]["Price"]))
+        ax.set_title("{} Last Price= {:f}".format(ss,json_summary["result"][0]["Price"]))
+        ss = ""
         self.canvas.draw()
 
     def nameBTC(self):
         self.listWidget.clear()
+        global btcList
         btcList=[]
+
         marketurl="https://bittrex.com/api/v1.1/public/getmarkets"
         market = requests.get(marketurl)
         json_market = market.json()
@@ -147,6 +171,7 @@ class Window(QDialog):
         self.listWidget.addItems(btcList)
 
     def nameETH(self):
+        global ethList
         ethList=[]
         marketurl="https://bittrex.com/api/v1.1/public/getmarkets"
         market = requests.get(marketurl)
@@ -161,6 +186,7 @@ class Window(QDialog):
         self.listWidget.addItems(ethList)
 
     def nameUSD(self):
+        global usdList
         usdList=[]
         marketurl="https://bittrex.com/api/v1.1/public/getmarkets"
         market = requests.get(marketurl)
