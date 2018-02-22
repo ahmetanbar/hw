3f2dbb2c4ec5f697d8dca8736e38197b63ecc943
ahmet anbar <mahsundemir@hacettepe.edu.tr>
2018-02-22 10:18:33 +0300

    silindi

--- hw4/database-cleaner.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("users.db")
-cursor = conn.cursor()
-cursor.execute('''CREATE TABLE userdata
-             (date text, trans text, symbol text, qty real, price real)''')
