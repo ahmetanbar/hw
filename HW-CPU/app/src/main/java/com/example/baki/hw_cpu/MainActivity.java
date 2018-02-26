b9811c26346b5a779c08f1f8fc05217e09104b2e
Muhamemd Baki <bakialmaci@gmail.com>
2018-02-26 20:25:12 +0300

    Jileeeeeeeettttt git add .!

--- HW-CPU/app/src/main/java/com/example/baki/hw_cpu/MainActivity.java
+++ HW-CPU/app/src/main/java/com/example/baki/hw_cpu/MainActivity.java
@@ -18,207 +18,289 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
 
 
-    public class MainActivity extends AppCompatActivity implements SensorEventListener {
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.text.Html;
+import android.widget.TextView;
+
+
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+
+
 
 
 //                  GENERAL
 
-        String model_name = Build.MODEL;
-        String board = Build.BOARD;
-        String brand = Build.BRAND;
-        String display = Build.DISPLAY;
-        String hardware = Build.HARDWARE;
-        String fingerprint = Build.FINGERPRINT;
-        String id = Build.ID;
-        String host = Build.HOST;
-        String manufacturer = Build.MANUFACTURER;
-        String user = Build.USER;
+    String model_name = Build.MODEL;
+    String board = Build.BOARD;
+    String brand = Build.BRAND;
+    String display = Build.DISPLAY;
+    String hardware = Build.HARDWARE;
+    String fingerprint = Build.FINGERPRINT;
+    String id = Build.ID;
+    String host = Build.HOST;
+    String manufacturer = Build.MANUFACTURER;
+    String user = Build.USER;
 
-        //      CPU USAGE
+    //      CPU USAGE
 
-        TextView textView;
-        ProcessBuilder processBuilder;
-        String Holder = "";
-        String[] DATA = {"/system/bin/cat", "/proc/cpuinfo"};
-        InputStream inputStream;
-        Process process;
-        byte[] byteArry;
+    TextView textView;
+    ProcessBuilder processBuilder;
+    String Holder = "";
+    String[] DATA = {"/system/bin/cat", "/proc/cpuinfo"};
+    InputStream inputStream;
+    Process process;
+    byte[] byteArry;
+    String [] my;
+    String [] my2;
+    String [] s2;
 
-        private TextView xText,yText,zText;
-        private Sensor mySensor;
-        private SensorManager SM;
+    private TextView xText,yText,zText;
+    private Sensor mySensor;
+    private SensorManager SM;
 
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-//                      SENSOR READING
+    String Log = "";
 
 
 
-            SM = (SensorManager)getSystemService(SENSOR_SERVICE);            //Create sensor manager
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
 
-            mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);            //Acc sensor
+        StringBuffer sb = new StringBuffer();
+        sb.append("abi: ").append(Build.CPU_ABI).append("n");
+        if (new File("/proc/cpuinfo").exists()) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(new File("/proc/cpuinfo")));
+                String aLine;
+                while ((aLine = br.readLine()) != null) {
+                    sb.append(aLine + "n");
+                }
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
 
-            SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);            //Register sensor listener
 
 
-            xText = (TextView)findViewById(R.id.textView22);
-            yText = (TextView)findViewById(R.id.textView23);
-            zText = (TextView)findViewById(R.id.textView24);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Create sensor manager
+
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        //Acc sensor
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //Register sensor listener
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //Assign Textview
+        xText = (TextView)findViewById(R.id.textView22);
+        yText = (TextView)findViewById(R.id.textView23);
+        zText = (TextView)findViewById(R.id.textView24);
 
 
 
 
 //               RAM USAGE
-
-            long freeSize = 0L;
-            long totalSize = 0L;
-            long usedSize = -1L;
-            try {
-                Runtime info = Runtime.getRuntime();
-                freeSize = info.freeMemory();
-                totalSize = info.totalMemory();
-                usedSize = totalSize - freeSize;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        long freeSize = 0L;
+        long totalSize = 0L;
+        long usedSize = -1L;
+        try {
+            Runtime info = Runtime.getRuntime();
+            freeSize = info.freeMemory();
+            totalSize = info.totalMemory();
+            usedSize = totalSize - freeSize;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
 
 //                      CPU TEMPERATURE
 
-            float temp = 0;
-            Process process;
-            try {
-                process = Runtime.getRuntime().exec("cat sys/class/thermal/thermal_zone0/temp");
-                process.waitFor();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                String line = reader.readLine();
-                temp = Float.parseFloat(line) / 1000.0f;
+        float temp = 0;
+        Process process;
+        try {
+            process = Runtime.getRuntime().exec("cat sys/class/thermal/thermal_zone0/temp");
+            process.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = reader.readLine();
+            temp = Float.parseFloat(line) / 1000.0f;
 
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-            TextView temp_text = (TextView) findViewById(R.id.textView21);
-            temp_text.setText("CPU Temperature: "+temp);
+        TextView temp_text = (TextView) findViewById(R.id.textView21);
+        temp_text.setText( Html.fromHtml("<b> <big>"+ "CPU Temperature:" + "</big> </b>"+ temp +"<br />"));
 
 
 //                      CPU READING
+        textView  = (TextView) findViewById(R.id.textView11);
+//        textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
+////        textView = (TextView) findViewById(R.id.textView11);
+        byteArry = new byte[1024];
 
-            textView = (TextView) findViewById(R.id.textView11);
-            byteArry = new byte[1024];
 
-            try {
-                processBuilder = new ProcessBuilder(DATA);
+        //set text style bold on current font
 
-                process = processBuilder.start();
 
-                inputStream = process.getInputStream();
 
-                while (inputStream.read(byteArry) != -1) {
+        try {
+            processBuilder = new ProcessBuilder(DATA);
 
-                    Holder = Holder + new String(byteArry);
-                }
+            process = processBuilder.start();
 
-                inputStream.close();
+            inputStream = process.getInputStream();
 
-            } catch (IOException ex) {
+            while (inputStream.read(byteArry) != -1) {
 
-                ex.printStackTrace();
+                Holder = Holder + new String(byteArry);
+                my = Holder.split("\n");
+
+
             }
 
-            textView.setText(Holder);
+            inputStream.close();
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+        }
+
+
+
+        String linetest="";
+
+        for (int i=0; i<my.length;i++){
+
+//            textView.setText(my[i]);
+
+            linetest=my[i];
+            int pointer = linetest.indexOf(":");
+            int finish=linetest.length();
+
+            String str = Integer.toString(pointer);
+            String str2 = Integer.toString(finish);
+            if (pointer!=(finish-1)){
+                my2 = my[i].split(":");
+                Log = Log + ("<b> <big>"+ my2[0] + "</big> </b>") + " : " + (my2[1]) + "\n\n";
+            } else{
+                Log = Log + ("<b> <big>"+ my[i] + "</big> </b>") + "\n\n";
+            }
+
+            textView.setText( Html.fromHtml(Log.replace("\n","<br />")));
+
+        }
+
+
 
 
 
 
 //                       TAB SETTINGS
+        TabHost th = (TabHost) findViewById(R.id.tabHost);
+        th.setup();
 
-            TabHost th = (TabHost) findViewById(R.id.tabHost);
-            th.setup();
+        TextView model_txt = (TextView) findViewById(R.id.textView1);
+        model_txt.setText( Html.fromHtml("<b> <big>"+ "Model:" + "</big> </b>"+ model_name +"<br />"));
 
-            TextView model_txt = (TextView) findViewById(R.id.textView1);
-            model_txt.setText("Model: " + model_name + "\n");
+        TextView board_txt = (TextView) findViewById(R.id.textView2);
+        board_txt.setText( Html.fromHtml("<b> <big>"+ "Board:" + "</big> </b>"+ board +"<br />"));
 
-            TextView board_txt = (TextView) findViewById(R.id.textView2);
-            board_txt.setText("Board: " + board + "\n");
+        TextView brand_txt = (TextView) findViewById(R.id.textView3);
+        brand_txt.setText( Html.fromHtml("<b> <big>"+ "Brand:" + "</big> </b>"+ brand +"<br />"));
 
-            TextView brand_txt = (TextView) findViewById(R.id.textView3);
-            brand_txt.setText("Brand: " + brand + "\n");
+        TextView display_txt = (TextView) findViewById(R.id.textView4);
+        display_txt.setText( Html.fromHtml("<b> <big>"+ "Display:" + "</big> </b>"+ display +"<br />"));
 
-            TextView display_txt = (TextView) findViewById(R.id.textView4);
-            display_txt.setText("Display: " + display + "\n");
+        TextView hardware_txt = (TextView) findViewById(R.id.textView5);
+        hardware_txt.setText( Html.fromHtml("<b> <big>"+ "Hardware:" + "</big> </b>"+ hardware +"<br />"));
 
-            TextView hardware_txt = (TextView) findViewById(R.id.textView5);
-            hardware_txt.setText("Hardware: " + hardware + "\n");
+        TextView fingerprint_txt = (TextView) findViewById(R.id.textView6);
+        fingerprint_txt.setText( Html.fromHtml("<b> <big>"+ "FingerPrint:" + "</big> </b>"+ fingerprint +"<br />"));
 
-            TextView fingerprint_txt = (TextView) findViewById(R.id.textView6);
-            fingerprint_txt.setText("Fingerprint: " + fingerprint + "\n");
+        TextView id_txt = (TextView) findViewById(R.id.textView7);
+        id_txt.setText( Html.fromHtml("<b> <big>"+ "ID:" + "</big> </b>"+ id +"<br />"));
 
-            TextView id_txt = (TextView) findViewById(R.id.textView7);
-            id_txt.setText("ID: " + id + "\n");
+        TextView host_txt = (TextView) findViewById(R.id.textView8);
+        host_txt.setText( Html.fromHtml("<b> <big>"+ "Host:" + "</big> </b>"+ host +"<br />"));
 
-            TextView host_txt = (TextView) findViewById(R.id.textView8);
-            host_txt.setText("Host: " + host + "\n");
+        TextView manufacturer_txt = (TextView) findViewById(R.id.textView9);
+        manufacturer_txt.setText( Html.fromHtml("<b> <big>"+ "Manufacturers:" + "</big> </b>"+ manufacturer +"<br />"));
 
-            TextView manufacturer_txt = (TextView) findViewById(R.id.textView9);
-            manufacturer_txt.setText("Manufactuer: " + manufacturer + "\n");
+        TextView user_txt = (TextView) findViewById(R.id.textView10);
+        user_txt.setText( Html.fromHtml("<b> <big>"+ "User:" + "</big> </b>"+ user +"<br />"));
 
-            TextView user_txt = (TextView) findViewById(R.id.textView10);
-            user_txt.setText("User: " + user + "\n");
+        TabHost.TabSpec spec = th.newTabSpec("Tag1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("GENERAL");
+        th.addTab(spec);
 
-            TabHost.TabSpec spec = th.newTabSpec("Tag1");
-            spec.setContent(R.id.tab1);
-            spec.setIndicator("GENERAL");
-            th.addTab(spec);
+        spec = th.newTabSpec("Tag2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("CPU");
+        th.addTab(spec);
 
-            spec = th.newTabSpec("Tag2");
-            spec.setContent(R.id.tab2);
-            spec.setIndicator("CPU");
-            th.addTab(spec);
-
-            spec = th.newTabSpec("Tag3");
-            spec.setContent(R.id.tab3);
-            spec.setIndicator("SENSORS");
-            th.addTab(spec);
+        spec = th.newTabSpec("Tag3");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("SENSORS");
+        th.addTab(spec);
 
 //                          USAGE
 
-            spec = th.newTabSpec("Tag4");
-            spec.setContent(R.id.tab4);
-            spec.setIndicator("USAGE");
-            th.addTab(spec);
+        spec = th.newTabSpec("Tag4");
+        spec.setContent(R.id.tab4);
+        spec.setIndicator("USAGE");
+        th.addTab(spec);
 
-            TextView  ram_txt = (TextView) findViewById(R.id.textView31);
-            ram_txt.setText("Ram Usage: "+usedSize);
+        TextView  ram_txt = (TextView) findViewById(R.id.textView31);
+        ram_txt.setText( Html.fromHtml("<b> <big>"+ "Ram Usage:" + "</big> </b>"+ usedSize +"<br />"));
 
-            TextView  total_ram_txt = (TextView) findViewById(R.id.textView32);
-            total_ram_txt.setText("Total Ram: "+totalSize);
-
-            TextView  free_ram_txt = (TextView) findViewById(R.id.textView33);
-            free_ram_txt.setText("Free Ram: "+freeSize);
+        TextView  total_ram_txt = (TextView) findViewById(R.id.textView32);
+        total_ram_txt.setText( Html.fromHtml("<b> <big>"+ "Total Ram:" + "</big> </b>"+ totalSize +"<br />"));
 
 
+        TextView  free_ram_txt = (TextView) findViewById(R.id.textView33);
+        free_ram_txt.setText( Html.fromHtml("<b> <big>"+ "Free Ram: " + "</big> </b>"+ freeSize +"<br />"));
 
 
-        }
-        
 
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            xText.setText("X Axis: " + event.values[0]);
-            yText.setText("Y Axis: " + event.values[1]);
-            zText.setText("Z Axis: " + event.values[2]);
-        }
-        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not in use
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        xText.setText( Html.fromHtml("<b> <big>"+ "X Axis: " + "</big> </b>"+ event.values[0] +"<br />"));
+        yText.setText( Html.fromHtml("<b> <big>"+ "Y Axis: " + "</big> </b>"+ event.values[1] +"<br />"));
+        zText.setText( Html.fromHtml("<b> <big>"+ "Z Axis: " + "</big> </b>"+ event.values[2] +"<br />"));
+    }
+}
 
 
 
