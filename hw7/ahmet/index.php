ae13f21aa64f85c4e0ae7428412b00768d0eb902
ahmetanbar <33193374+ahmetanbar@users.noreply.github.com>
2018-03-28 23:52:36 +0300

    Create index.php

---
+++ hw7/ahmet/index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="./index.css">
+  <meta charset="UTF-8"/>
+</head>
+<body>
+<div>
+
+<header>
+  <h1>What About?</h1>
+</header>
+
+<article>
+  <form action="/action_page.php">
+  Name:<br>
+  <input type="text" name="Name">
+  <br><br>
+  Surname:<br>
+  <input type="text" name="Surname" >
+  <br><br>
+  Password:<br>
+  <input type="password" name="psw" size="35">
+  <br><br>
+  Password:<br>
+  <input type="password" name="psw">
+  <br><br>
+  Birthday:<br>
+  <input id="date" type="date">
+  <br><br>
+  <input type="radio" name="gender" value="male" checked> Male
+  <input type="radio" name="gender" value="female">Female <br>
+  <br><br>
+  <input type="submit" onclick="myFunction()" value="Submit">
+</form>
+</article>
+
+<footer>Copyright &copy; BAK Software</footer>
+</div>
+</body>
+</html>
