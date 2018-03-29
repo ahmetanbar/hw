9bc0b052dc3b9954d36787d033c2acd255b25486
Muhammed Baki Almacı <bakialmaci@gmail.com>
2018-03-30 00:21:45 +0300

    Create profile.php

---
+++ hw7/baki/profile.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="profile.css">
+    <link rel="stylesheet" type="text/html" href="homepage.php">
+    <title>Welcome to Soceanic</title>
+</head>
+<body class="profile-page">
+
+<video autoplay muted loop id="myVideo">
+    <source src="cloud.mp4" type="video/mp4">
+</video>
+
+    <div class="top-bar">
+            <a id="bar-button" href="homepage.php">
+                HomePage
+            </a>
+            <a id="bar-button" href="profile.php">
+                Profile
+            </a>
+            <a id="bar-button" href="homepage.php">
+                Logout
+            </a>
+    </div>
+
+    <div class="user-page">
+            <a> Welcome to [$username]</a>
+    </div>
+
+
+</body>
+</html>
