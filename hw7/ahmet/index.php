256624afb1f76dc8c61dd1d309f4e4d8077a04ab
ahmet anbar <ahmetanbar@hacettepe.edu.tr>
2018-03-31 01:48:18 +0300

    setted user log in,sign up,cookies done clear,and some deficients eliminated

--- hw7/ahmet/index.php
+++ hw7/ahmet/index.php
@@ -23,6 +23,21 @@ echo 'Message: ' .$e->getMessage();
 </head>
 <body>
   <?php
+  function generateRandomString($length = 52) {
+    return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+  }
+    if(count($_COOKIE)!=0){
+    $key_id= $_COOKIE['key_id'];
+    $user_id= $_COOKIE['user_id'];
+    $sql = "SELECT key_id,user_id FROM cookie WHERE key_id='$key_id' AND user_id='$user_id'";
+    $result = $conn->query($sql) or die($conn->error);
+    $row = $result->fetch_assoc();
+    if(count($row)!=0)
+    {
+      header("Location:profile.php"); /* Redirect browser */
+    }
+  }
+
   function login_verify() {
     $servername = "localhost";
     $username = "root";
@@ -38,33 +53,92 @@ echo 'Message: ' .$e->getMessage();
  echo 'Message: ' .$e->getMessage();
 }
     if(count($_POST)!=0){
-    $email=$_POST['emailorphone'];
-    $password=$_POST['password_2'];
-    $sql = "SELECT email,parola,id FROM users WHERE email='$email' AND parola='$password'";
-    $result = $conn->query($sql) or die($conn->error);
-    $row = $result->fetch_assoc();
-    if(count($row)!=0){
-      function generateRandomString($length = 52) {
-        return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+    try{
+      if(array_key_exists("emailorphone",$_POST) or array_key_exists("password_2",$_POST)){
+        if($_POST['emailorphone']==NULL or $_POST['password_2']==NULL){
+          echo "Complete all of them!!cCc";
+        }
+        else{
+          $email=$_POST['emailorphone'];
+          $password=$_POST['password_2'];
+          $sql = "SELECT email,parola,id FROM users WHERE email='$email' AND parola='$password'";
+          $result = $conn->query($sql) or die($conn->error);
+          $row = $result->fetch_assoc();
+          if(count($row)!=0){
+            $key_id=generateRandomString();
+            $user_id=$row["id"];
+            setcookie('key_id',$key_id);
+            setcookie('user_id',$user_id);
+            $sql2 = "INSERT INTO cookie (key_id,user_id) VALUES('$key_id','$user_id')";
+            if ($conn->query($sql2) === TRUE) {
+            echo("succesful");
+            }
+            else{
+              echo("unsuccessful");
+            }
+            header("Location:profile.php"); /* Redirect browser */
+        }
+        else{
+          echo "Wrong password or username";
+        }
       }
-      $key_id=generateRandomString();
-      $user_id=$row["id"];
-      setcookie('key_id',$key_id);
-      setcookie('user_id',$user_id);
-      $sql2 = "INSERT INTO cookie (key_id,user_id) VALUES('$key_id','$user_id')";
-      if ($conn->query($sql2) === TRUE) {
-      echo("succesful");
+    }
+  }
+  catch(Exception $e){
+echo 'Message: ' .$e->getMessage();
+  }
+  try{
+    if(array_key_exists("name",$_POST) or array_key_exists("surname",$_POST) or array_key_exists("psw",$_POST) or array_key_exists("email",$_POST) or array_key_exists("username",$_POST)){
+      if($_POST['name']==NULL or $_POST['surname']==NULL or $_POST['psw']==NULL or $_POST['email']==NULL or $_POST['username']==NULL){
+        echo("Complete all of block!!!cCc");
       }
       else{
-        echo("unsuccessful");
+   	    $email=$_POST['email'];
+        $username=$_POST['username'];
+        $flag_signup=null;
+        $sql = "SELECT email,username FROM users WHERE email='$email' AND username='$username'";
+        $result = $conn->query($sql) or die($conn->error);
+        $row = $result->fetch_assoc();
+        // if ($conn->query($sql) === TRUE) {
+        //   $flag_signup=True;
+        // }
+        if(count($row)==0){
+          $name=$_POST['name'];
+     	    $surname=$_POST['surname'];
+          $password=$_POST['psw'];
+          $gender=$_POST['gender'];
+          $sql = "INSERT INTO users (name,surname,email,parola,gender,username) VALUES('$name','$surname','$email','$password','$gender','$username')";
+          if ($conn->query($sql) === TRUE) {
+            $sql = "SELECT id FROM users WHERE username='$username'";
+            $result = $conn->query($sql) or die($conn->error);
+            $row = $result->fetch_assoc();
+            echo $row['id'];
+            $key_id=generateRandomString();
+            $user_id=$row["id"];
+            setcookie('key_id',$key_id);
+            setcookie('user_id',$user_id);
+            $sql = "INSERT INTO cookie (key_id,user_id) VALUES('$key_id','$user_id')";
+            if ($conn->query($sql) === TRUE) {
+            }
+            header("Location:profile.php"); /* Redirect browser */
+          }
+          else{
+            echo "accured a error!!!cCc";
+          }
+        }
+        else{
+          echo "we have already the username or mail";
+        }
       }
-      header("Location:profile.php"); /* Redirect browser */
     }
-    else{
-      echo "Wrong password or username";
+    else if(!array_key_exists("emailorphone",$_POST) and !array_key_exists("gender",$_POST)){
+      echo "bad connected";
     }
-  }
-  }
+    }
+    catch(Exception $e){
+    }
+}
+}
   login_verify();
   ?>
 
@@ -77,7 +151,6 @@ echo 'Message: ' .$e->getMessage();
     <form action="" method="post">
     <input style="width: 350px;" type="text" name="emailorphone" placeholder="Phone or Email"><input type="password" name="password_2" placeholder="Password" style="width: 350px;">
     <br>
-
     <input type="submit" value="Login" style="padding: 16px 32px; font-size:1.5em;">
   </form>
   </article>
@@ -85,10 +158,11 @@ echo 'Message: ' .$e->getMessage();
 </div>
 <div>
 <article>
-  <form action="welcome.php" method="post">
+  <form action="" method="post">
   <input style="width: 240px;" type="text" name="name" placeholder="Name"><input type="text" name="surname" placeholder="Surname" style="width: 240px;">
   <br>
   <input type="text" name="email" placeholder="Phone or Email">
+  <br><input type="text" name="username" placeholder="Username">
   <br><input type="password" name="psw" placeholder="Password">
   <br><br>
   <br><input type="radio" name="gender" value="male" style="font-size:1.5em;" checked> <label style="font-size: 30px;">Male</label>
--- hw7/ahmet/profile.php
+++ hw7/ahmet/profile.php
@@ -20,6 +20,7 @@
       $key_id= $_COOKIE['key_id'];
       $user_id= $_COOKIE['user_id'];
     }
+
       if(count($_POST)!=0){
         $sql = "DELETE FROM cookie WHERE user_id='$user_id' AND key_id='$key_id'";
           if (mysqli_query($conn, $sql)) {
@@ -28,7 +29,6 @@
 
         header("Location:index.php"); /* Redirect browser */
       }
-
       $sql = "SELECT key_id,user_id FROM cookie WHERE key_id='$key_id' AND user_id='$user_id'";
       $result = $conn->query($sql) or die($conn->error);
       $row = $result->fetch_assoc();
@@ -39,6 +39,7 @@
         $row = $result->fetch_assoc();
         echo "Hi!".$row["email"]."Welcome to your own page.";
         ?> <br><br>
+        <h1>Don't Worry.You are the first of the hearts as design</h1>
         <form action="" method="post">
         <input type="submit" value="Log Out" style="padding: 16px 32px; font-size:1.5em;" name='username'>
         </form>
