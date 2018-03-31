1b0738416e7fe2bab7b487a407922db45c87e365
Muhammed Baki Almacı <bakialmaci@gmail.com>
2018-03-31 18:11:07 +0300

    Update profile.php

--- hw7/baki/profile.php
+++ hw7/baki/profile.php
@@ -4,10 +4,27 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="profile.css">
     <link rel="stylesheet" type="text/html" href="homepage.php">
-    <title>Welcome to Soceanic</title>
+    <title>Welcome to Socean</title>
 </head>
 <body class="profile-page">
+<?php
+$auth= $_COOKIE['auth'];
+$conn = new mysqli("localhost", "root", "", "users");
+$read = $conn->query("SELECT * FROM cookies WHERE cookie='" . $auth . "'");
+$list = mysqli_fetch_array($read);
+$username="";
+if(!empty($list)){
 
+    $userid = $list["user_id"];
+    $read = $conn->query("SELECT * FROM users_table WHERE id='" . $userid . "'");
+    $list = mysqli_fetch_array($read);
+    $username = "Logout-".$list[1];
+
+}
+else{
+    header("Location: ./homepage.php");
+}
+?>
 <video autoplay muted loop id="myVideo">
     <source src="cloud.mp4" type="video/mp4">
 </video>
@@ -19,25 +36,9 @@
             <a id="bar-button" href="profile.php">
                 Profile
             </a>
-            <a id="bar-button" href="homepage.php">
+            <a id="bar-button" href="logout.php">
                 <?php
-                $auth= $_COOKIE['auth'];
-                $conn = new mysqli("localhost", "root", "", "users");
-                $read = $conn->query("SELECT * FROM cookies WHERE cookie='" . $auth . "'");
-                $list = mysqli_fetch_array($read);
-
-                if(!empty($list)){
-
-                    $userid = $list["user_id"];
-                    $read = $conn->query("SELECT * FROM users_table WHERE id='" . $userid . "'");
-                    $list = mysqli_fetch_array($read);
-                    $username = $list[1];
-                    echo "Logout-$username";
-
-                }
-                else{
-                    header("Location: ./homepage.php");
-                }
+                echo $username;
                 ?>
             </a>
 
