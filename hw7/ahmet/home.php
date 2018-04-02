c7ea1f6ef57086ee1f20dcf759c6521bfee5d4aa
ahmet anbar <ahmetanbar@hacettepe.edu.tr>
2018-04-03 02:58:43 +0300

    design created will add block of article

---
+++ hw7/ahmet/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Code Note</title>
+  <link rel="stylesheet" type="text/css" href="./home.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <div class="content">
+      <div class="header">
+        <h1>Code Note</h1>
+      </div>
+
+      <div class="topnav">
+        <a href="#">Home</a>
+        <a href="#">Archive</a>
+        <a href="https://google.com">About</a>
+        <a style="float:right;" href="#">Sign Up</a>
+        <a style="float:right;" href="#">Log In</a>
+      </div>
+
+      <div class="column">
+        <h2>Code Day</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+      </div>
+      <div class="column">
+        <h2>Day of Code</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+      </div>
+      <div class="column">
+        <h2>Code's Day</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+      </div>
+  </div>
+</body>
+</html>
