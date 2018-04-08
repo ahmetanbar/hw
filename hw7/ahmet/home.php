58aa48fee77fdea3e4de3b403d75d8d3bea871a9
ahmet anbar <ahmetanbar@hacettepe.edu.tr>
2018-04-09 02:57:29 +0300

    added signup, setted sizing for very small

--- hw7/ahmet/home.php
+++ hw7/ahmet/home.php
@@ -7,7 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+
   <div class="main">
+
       <div class="header">
         <h1>Code Note</h1>
       </div>
@@ -15,33 +17,33 @@
         <a href="#">Home</a>
         <a href="#">Archive</a>
         <a href="https://google.com">About</a>
-        <a style="float:right;" href="#">Sign Up</a>
+        <a style="float:right;" href="signup.php">Sign Up</a>
         <a style="float:right;" href="#">Log In</a>
       </div>
       <br>
         <?php $number_box=5;
         for($i=0; $i<=$number_box ;$i++) { ?>
           <div class="content">
-                <div class="art_head">
-                  <h2><a href="https://google.com">Code Day</a></h2>
-                </div>
-                <div class="article"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque Lorem ipsum dolor sit amet,ass consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec nequeLorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec nequeLorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.
-                <a href="https://google.com">More▷</a></p>
+              <div class="art_head">
+                <h3><a href="https://google.com">Code Day</a></h3>
               </div>
-              <div class="info">
-                <i style="float:left;" class="material-icons md-18">date_range</i>
-                <a style="float:left;" href="https://google.com">12.01.2017</a>
-                <i style="float:left;" class="material-icons md-18" >account_balance</i>
-                <a style="float:left;" href="https://google.com">Java</a>
+              <div class="article"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque Lorem ipsum dolor sit amet,ass consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec nequeLorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec nequeLorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+              <a href="https://google.com">More▷</a></p>
+            </div>
+            <div class="info">
+              <i style="float:left;" class="material-icons md-18">date_range</i>
+              <a style="float:left;" href="https://google.com">12.01.2017</a>
+              <i style="float:left;" class="material-icons md-18" >account_balance</i>
+              <a style="float:left;" href="https://google.com">Java</a>
 
-                <i style="float:left;" class="material-icons md-18" >account_circle </i>
-                <a style="float:left; " href="https://google.com">Ahmet Anbar</a>
-                <a style="float:right;" href="https://google.com" >Viewing:5</a>
-                <i style="float:right;" class="material-icons md-18">assessment</i>
-                <a style="float:right;" href="https://google.com">Comments:7</a>
-                <i style="float:right;" class="material-icons md-18">comment</i>
-                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-              </div>
+              <i style="float:left;" class="material-icons md-18" >account_circle </i>
+              <a style="float:left; " href="https://google.com">Ahmet Anbar</a>
+              <a style="float:right;" href="https://google.com" >Viewing:5</a>
+              <i style="float:right;" class="material-icons md-18">assessment</i>
+              <a style="float:right;" href="https://google.com">Comments:7</a>
+              <i style="float:right;" class="material-icons md-18">comment</i>
+              <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+            </div>
           </div>
             <br>
           <?php } ?>
---
+++ hw7/ahmet/signup.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Code Note</title>
+  <link rel="stylesheet" type="text/css" href="./signup.css">
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <div class="main">
+      <div class="header">
+        <h1>Code Note</h1>
+      </div>
+      <div class="topnav">
+        <a href="./deneme.php">Home</a>
+        <a href="#">Archive</a>
+        <a href="https://google.com">About</a>
+        <a style="float:right;" href="#">Log In</a>
+      </div>
+      <br>
+          <div class="content">
+            <br>
+            <h1>Join CodeNote</h1>
+
+            <form action="" method="post" autocomplete="off" />
+            <input style="width: 180px;" type="text" name="name" placeholder="Name"><input type="text" name="surname" placeholder="Surname" style="width: 180px;">
+            <br>
+            <input type="text" name="email" placeholder="Phone or Email">
+            <br><input type="text" name="username" placeholder="Username">
+            <br><input type="password" name="psw" placeholder="Password">
+            <br>
+            <br><input type="radio" name="gender" value="male" style="font-size:1.2em;" checked> <label style="font-size: 18px;">Male</label>
+            <input type="radio" name="gender" value="female"; ><label style="font-size: 18px;">Female</label> <br>
+            <br>
+            <input type="submit" value="Sign Up" style="padding: 14px 30px; font-size:1.0em;">
+          </form>
+          </div>
+            <br>
+        <br>
+
+      <footer>Copyleft &copy;</footer>
+  </div>
+</body>
+</html>
