98aac7c0b2f40a0d415352d83b8037ad034abd32
ahmet anbar <ahmetanbar@hacettepe.edu.tr>
2018-04-11 23:51:27 +0300

    sessions assigned, working for clear code

--- hw7/ahmet/signup.php
+++ hw7/ahmet/signup.php
@@ -12,67 +12,73 @@
       function control_post(){
         if(count($_POST)!=0){
           if(array_key_exists("name",$_POST) and array_key_exists("surname",$_POST) and array_key_exists("email",$_POST) and array_key_exists("username",$_POST) and array_key_exists("psw",$_POST) and array_key_exists("gender",$_POST)){
+            $logs=array("name"=>"","surname"=>"","email"=>"","username"=>"","password"=>"","foot_note"=>"");
             if($_POST['name']!=NULL and $_POST['surname']!=NULL and $_POST['email']!=NULL and $_POST['username']!=NULL and $_POST['psw']!=NULL and $_POST['gender']!=NULL){
-              echo("icerdesin");
-              $varify_cont=0;
-              $logs=array("name"=>"","surname"=>"","email"=>"","username"=>"","password"=>"");
-              //name regular expression control
-              $subject = $_POST['name'];
-              $pattern = '/^[a-zA-Z]{3,20}$/';
-              if(preg_match($pattern,$subject)){
-                $varify_cont+=1;
-              }
-              else{
-                $logs['name']="Write a real name!";
-              }
-              //surname regular expression control
-              $subject = $_POST['surname'];
-              $pattern = '/^[a-zA-Z]{2,20}$/';
-              if(preg_match($pattern,$subject)){
-                $varify_cont+=1;
-              }
-              else{
-                $logs['surname']="Write a real surname!";
-              }
-              //email regular expression control
-              if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-                $varify_cont+=1;
-              }
-              else{
-                $logs['email']="Write a valid email!";
-              }
-              //username regular expression control
-              $subject = $_POST['username'];
-              $pattern = '/^[a-zA-Z0-9_-]{3,20}$/';
-              if(preg_match($pattern,$subject)){
-                $varify_cont+=1;
-              }
-              else{
-                if(strlen($subject)>=3 and strlen($subject)<=20)
-                $logs['username']="You can use only letter, number, '_' and '-'!";
-                else if(strlen($subject)<3 or strlen($subject)>20)
-                $logs['username']="Username must has distance 3-20!";
-              }
-              //password regular expression control
-              $subject = $_POST['psw'];
-              $pattern='/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{10,30}$/';
-              if(preg_match($pattern,$subject)){
-                $varify_cont+=1;
-              }
-              else{
-                if(strlen($subject)>=10 and strlen($subject)<=30)
-                $logs['password']="Parola must contain lowercase,uppercase,number!";
-                else if(strlen($subject)<10 or strlen($subject)>30)
-                $logs['password']="Parola must has distance 10-30!";
-              }
-              return $logs;
+                $varify_cont=0;
+                //name regular expression control
+                $subject = $_POST['name'];
+                $_SESSION["name"]=$subject;
+                $pattern = '/^[a-zA-Z]{3,20}$/';
+                if(preg_match($pattern,$subject)){
+                  $varify_cont+=1;
+                }
+                else{
+                  $logs['name']="Write a real name!";
+                }
+                //surname regular expression control
+                $subject = $_POST['surname'];
+                $_SESSION["surname"]=$subject;
+                $pattern = '/^[a-zA-Z]{2,20}$/';
+                if(preg_match($pattern,$subject)){
+                  $varify_cont+=1;
+                }
+                else{
+                  $logs['surname']="Write a real surname!";
+                }
+                //email regular expression control
+                $_SESSION["email"]=$subject;
+                if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+                  $varify_cont+=1;
+                }
+                else{
+                  $logs['email']="Write a valid email!";
+                }
+                //username regular expression control
+                $subject = $_POST['username'];
+                $_SESSION["username"]=$subject;
+                $pattern = '/^[a-zA-Z0-9_-]{3,20}$/';
+                if(preg_match($pattern,$subject)){
+                  $varify_cont+=1;
+                }
+                else{
+                  if(strlen($subject)>=3 and strlen($subject)<=20)
+                  $logs['username']="You can use only letter, number, '_' and '-'!";
+                  else if(strlen($subject)<3 or strlen($subject)>20)
+                  $logs['username']="Username must has length of 3-20!";
+                }
+                //password regular expression control
+                $subject = $_POST['psw'];
+                $pattern='/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{10,30}$/';
+                if(preg_match($pattern,$subject)){
+                  $varify_cont+=1;
+                  $_SESSION["psw"]=$subject;
+                }
+                else{
+                  if(strlen($subject)>=10 and strlen($subject)<=30)
+                  $logs['password']="Password must contain lowercase,uppercase,number!";
+                  else if(strlen($subject)<10 or strlen($subject)>30)
+                  $logs['password']="Password must has length of 10-30!";
+                }
             }
             else{
-              // echo("complete of all");
+                $logs['foot_note']="Complete All of Them!";
             }
+            return $logs;
           }
         }
       }
+    //_SESSION can be move other place???
+    $_SESSION=array("name"=>"","surname"=>"","email"=>"","username"=>"","password"=>"");
     $logs=control_post();
   ?>
   <div class="main">
@@ -91,28 +97,24 @@
           <h1>Join CodeNote</h1>
 
           <form action="" method="post" autocomplete="off" />
-          <?php if($logs['name']){?>
-          <span style="color:#c30000;"><?php echo $logs['name'];?></span><br> <?php } ?>
-          <?php if($logs['surname']){?>
-          <span style="color:#c30000;"><?php echo $logs['surname'];?></span><br> <?php } ?>
-          <input style="width: 180px;" type="text" name="name" placeholder="Name"><input type="text" name="surname" placeholder="Surname" style="width: 180px;">
-          <br>
-          <?php if($logs['email']){?>
-          <span style="color:#c30000;"><?php echo $logs['email'];?></span><br> <?php } ?>
-          <input type="text" name="email" placeholder="Phone or Email">
-          <br>
-          <?php if($logs['username']){?>
-          <span style="color:#c30000;"><?php echo $logs['username'];?></span><br> <?php } ?>
-          <input type="text" name="username" placeholder="Username">
-          <br>
-          <?php if($logs['password']){?>
-          <span style="color:#c30000;"><?php echo $logs['password'];?></span><br> <?php } ?>
-          <input type="password" name="psw" placeholder="Password">
-          <br>
-          <br><input type="radio" name="gender" value="male" style="font-size:1.2em;" checked> <label style="font-size: 18px;">Male</label>
-          <input type="radio" name="gender" value="female"; ><label style="font-size: 18px;">Female</label> <br>
-          <br>
-          <input type="submit" value="Sign Up" style="padding: 14px 30px; font-size:1.0em;">
+            <?php echo ($logs['foot_note']) ? '<span style="color:#c30000;">'.$logs['foot_note'].'</span><br>':''; ?>
+            <?php echo ($logs['name']) ? '<span style="color:#c30000;">'.$logs['name'].'</span><br>':''; ?>
+            <?php echo ($logs['surname']) ? '<span style="color:#c30000;">'.$logs['surname'].'</span><br>':''; ?>
+            <input value="<?php echo ($_SESSION['name']) ? $_SESSION['name'] : '' ;?>" style="width: 180px;" type="text" name="name" placeholder="Name"><input value="<?php echo ($_SESSION['surname']) ? $_SESSION['surname'] : '' ;?>" type="text" name="surname" placeholder="Surname" style="width: 180px;">
+            <br>
+            <?php echo ($logs['email']) ? '<span style="color:#c30000;">'.$logs['email'].'</span><br>':''; ?>
+            <input value="<?php echo ($_SESSION['email']) ? $_SESSION['email'] : '' ;?>" type="text" name="email" placeholder="Phone or Email">
+            <br>
+            <?php echo ($logs['username']) ? '<span style="color:#c30000;">'.$logs['username'].'</span><br>':''; ?>
+            <input value="<?php echo ($_SESSION['username']) ? $_SESSION['username'] : '' ;?>" type="text" name="username" placeholder="Username">
+            <br>
+            <?php echo ($logs['password']) ? '<span style="color:#c30000;">'.$logs['password'].'</span><br>':''; ?>
+            <input value="<?php echo ($_SESSION['password']) ? $_SESSION['password'] : '' ;?>" type="password" name="psw" placeholder="Password">
+            <br>
+            <br><input type="radio" name="gender" value="male" style="font-size:1.2em;" checked> <label style="font-size: 18px;">Male</label>
+            <input type="radio" name="gender" value="female"; ><label style="font-size: 18px;">Female</label> <br>
+            <br>
+            <input type="submit" value="Sign Up" style="padding: 14px 30px; font-size:1.0em;">
           </form>
       </div>
       <br>
