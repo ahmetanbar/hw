b0c72a6b249427c25bc625f07e3f09528ba6d5c8
Muhammed Baki Almacı <bakialmaci@gmail.com>
2018-04-13 18:24:42 +0300

    Create login.php

---
+++ hw7/baki/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="login.css">
+
+    <meta charset="UTF-8">
+    <title>Socean</title>
+</head>
+<body>
+
+
+
+    <div class="form" >
+        <form method="post">
+
+            <input  id="login-input" name="username_login" type="text"     placeholder="username"/>
+            <input  id="login-input" name="password_login" type="password" placeholder="password"/>
+            <button id="login-btn"   name="btn" type="submit" value="login-btn-v" >LOGIN </button>
+        </form>
+
+    </div>
+
+
+</body>
+</html>
