2bed01ffde1097607e297bc8ec9187b182fe1d25
ahmet anbar <ahmetanbar@hacettepe.edu.tr>
2018-05-01 12:38:21 +0300

    setting of log comments is done

--- hw7/ahmet/article.php
+++ hw7/ahmet/article.php
@@ -62,7 +62,7 @@
     function get_comment(){
       $status="approve";
       $conn=connect_db();
-      $stmt = $conn->prepare("SELECT * FROM comments WHERE article_id=? and status=? order by id desc");
+      $stmt = $conn->prepare("SELECT comments.* , user.name,user.surname FROM comments INNER JOIN users user ON comments.user_id = user.id WHERE comments.article_id=? and comments.status=? order by id desc");
       $stmt->bind_param("is", $_GET['id'],$status);
       $stmt->execute();
       $result = $stmt->get_result();
@@ -78,46 +78,25 @@
     }
     function comment_post(){
       if(count($_POST)!=0){
-        if(array_key_exists("namesurname",$_POST) and array_key_exists("email",$_POST) and array_key_exists("comment",$_POST)){
-          $logs=array("namesurname"=>"" , "email"=>"" , "foot_note"=>"" , "input_control"=>"" , "value_name"=>"" , "value_comment"=>"" , "value_email"=>"");
-          if($_POST['namesurname']!=NULL and $_POST['email']!=NULL and $_POST['comment']!=NULL){
+        if(array_key_exists("comment",$_POST)){
+          $logs=array("value_comment"=>"","foot_note"=>"");
+          if( $_POST['comment']!=NULL){
             $verify_cont=0;
-            $logs['value_comment']=$_POST['comment'];
-            //name regular expression control
-            $subject = $_POST['namesurname'];
-            $pattern = '/^[a-zA-Z0-9çÇğĞıIiİöÖŞşÜü_-]{3,20}$/';
-            if(preg_match($pattern,$subject)){
-              $verify_cont+=1;
-              $logs['value_name']=$subject;
-            }
-            else{
-              $logs['namesurname']="Write a real name!";
-            }
-            //email regular expression control
-            if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-              $verify_cont+=1;
-              $logs['value_email']=$subject;
-            }
-            else{
-              $logs['email']="Write a valid email!";
-            }
-            //after control input
-            if($verify_cont==2){
-            $logs["value_name"]=""; $logs["value_comment"]=""; $logs["value_email"]="";
+            // $logs['value_comment']=$_POST['comment'];
+
+            $logs["value_comment"]="";
+
             $conn =connect_db();
 
-            $email=strtolower($_POST['email']);
-            $namesurname=$_POST['namesurname'];
             $comment=nl2br($_POST['comment']);
             $article_id=$_GET['id'];
             date_default_timezone_set("Europe/Istanbul");
             $datetime=date("y.m.d H:i");
-            $user_id=0;
+            $user_id=$_SESSION['id'];
             $status="notapprove";
-            $stmt = $conn->prepare("INSERT INTO comments (user_id,article_id,namesurname,email,comment,datetime,status) VALUES(?,?,?,?,?,?,?)");
-            $stmt->bind_param("iisssss", $user_id, $article_id,$namesurname,$email,$comment,$datetime,$status);
+            $stmt = $conn->prepare("INSERT INTO comments (user_id,article_id,comment,datetime,status) VALUES(?,?,?,?,?)");
+            $stmt->bind_param("iisss", $user_id, $article_id,$comment,$datetime,$status);
             $stmt->execute();
-            }
           }
           else{
             $logs['foot_note']="Complete All!";
@@ -127,9 +106,11 @@
       }
     }
     session_start();
+
     $cookie_know=control_cookie();
     $get_method=get_kind();
     $logs=comment_post();
+
 
   ?>
   <?php include 'topnav.php';?>
@@ -152,6 +133,9 @@
       $result=get_article($_GET['id']);
       if($result->num_rows==0)
         header("Location:./home.php");
+    }
+    else{
+      header("Location:./home.php");
     }
       for($i=0;$i<$number_box;$i++) {
         $row = $result->fetch_assoc();
@@ -193,7 +177,7 @@
               <div class="comments">
                   <i style="float:left;" class="material-icons md-80">person</i>
                 <div class="art_head">
-                  <h2><?php echo($row['namesurname']); ?></a></h2>
+                  <h2><a href="./profile.php?id=<?php echo($row['user_id']); ?>"><?php echo($row['name'].' '.$row['surname']); ?></a></h2>
                   <h4><?php echo(date('d-m-Y, H:i', strtotime($row['datetime']))); ?></h4>
                 </div>
 
@@ -203,20 +187,23 @@
                 <br>
               <?php }
 
+
           if($get_method=="id"){ ?>
             <div class="commentbox">
               <form method="post" autocomplete="off" accept-charset="UTF-8" action="">
                 <?php echo ($logs['foot_note']) ? '<span style="color:#c30000;">'.$logs['foot_note'].'</span><br>':''; ?>
                 <textarea id="comment" name="comment" placeholder="Write your comment.." style="height:200px"><?php echo ($logs['value_comment']) ? $logs['value_comment'] : '' ;?></textarea>
-                <?php echo ($logs['namesurname']) ? '<span style="color:#c30000;">'.$logs['namesurname'].'</span><br>':''; ?>
-                <input value="<?php echo ($logs['value_name']) ? $logs['value_name'] : '' ;?>" type="text" id="namesurname" name="namesurname" placeholder="Name,Surname*">
-                <label for="lname"></label>
-                <?php echo ($logs['email']) ? '<span style="color:#c30000;">'.$logs['email'].'</span><br>':''; ?>
-                <input value="<?php echo ($logs['value_email']) ? $logs['value_email'] : '' ;?>" type="text" id="email" name="email" placeholder="E-mail* (It will not be seen.)">
-                <input type="submit" value="Send">
+
+
+                <?php echo($cookie_know['flag']) ? '<input type="submit" value="Send">':'<span style="color:#c30000; text-align:auto; ">For comment you should <a color:red href="profile.php">Log In</a></span>'; ?>
+
               </form>
             </div>
-        <?php } ?>
+      <?php
+      }
+      else{
+
+      } ?>
     <?php include 'footer.php';?>
   </div>
 </body>
