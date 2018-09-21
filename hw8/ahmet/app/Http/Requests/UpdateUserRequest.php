911ac3b3a0f4e995d74112c314869cab4fced9fa
ahmet anbar <ahmetanbar@hacettepe.edu.tr>
2018-09-21 01:44:51 -0400

    user updating, adding is updated in admin panel

---
+++ hw8/ahmet/app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Auth;
+use App\Http\Requests\Request;
+use Illuminate\Foundation\Http\FormRequest;
+
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'surname' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users,username,'.request()->id.'',
+            'email' => 'required|string|email|max:255|unique:users,email,'.request()->id.'',
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //
+        ];
+    }
+}
